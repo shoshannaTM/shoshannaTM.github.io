c784eab19dee3d1bd280a2f538c3276adae8f3e5
shoshannaTM <sctm14@gmail.com>
2026-03-18 16:36:23 -0700

    Added Libre Closet project

--- gulpfile.js
+++ gulpfile.js
@@ -2,41 +2,101 @@ var gulp = require('gulp');
 var csso = require('gulp-csso');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
 var plumber = require('gulp-plumber');
-var cp = require('child_process');
+var fs = require('fs');
 var imagemin = require('gulp-imagemin');
 var browserSync = require('browser-sync');
 
-var jekyllCommand = (/^win/.test(process.platform)) ? 'jekyll.bat' : 'jekyll';
+// Site variables from _config.yml
+var siteConfig = {
+  title: "Shoshanna's Portfolio",
+  description: "This is a portfolio website to document and share code and UX projects I've worked on.",
+  username: "Shoshanna Thomas-McCue",
+  user_title: "Software Developer",
+  user_description: "Hi, I'm Shoshanna. I'm certified in UX Design and Research and hold a Bachelor of Science in Software Development. I love building thoughtful software that makes a positive impact on those who use it.",
+  email: "contact@shoshannatm.dev",
+  github_username: "shoshannaTM",
+  baseurl: ""
+};
 
 /*
- * Build the Jekyll Site
- * runs a child process in node that runs the jekyll commands
+ * Build HTML by inlining Jekyll includes and resolving Liquid variables.
+ * Replaces Jekyll entirely — no Ruby required.
  */
-gulp.task('jekyll-build', function (done) {
-	return cp.spawn(jekyllCommand, ['build'], {stdio: 'inherit'})
-		.on('close', done);
+function buildHtml(done) {
+  var head     = fs.readFileSync('_includes/head.html', 'utf8');
+  var header   = fs.readFileSync('_includes/header.html', 'utf8');
+  var about    = fs.readFileSync('_includes/about.html', 'utf8');
+  var projects = fs.readFileSync('_includes/projects.html', 'utf8');
+  var footer   = fs.readFileSync('_includes/footer.html', 'utf8');
+  var layout   = fs.readFileSync('_layouts/default.html', 'utf8');
+
+  var html = '<!DOCTYPE html>\n<html lang="en">\n' + layout + '\n</html>';
+
+  // Inline includes
+  html = html.replace(/\{%\s*include head\.html\s*%\}/g, head);
+  html = html.replace(/\{%\s*include header\.html\s*%\}/g, header);
+  html = html.replace(/\{%\s*include about\.html\s*%\}/g, about);
+  html = html.replace(/\{%\s*include projects\.html\s*%\}/g, projects);
+  html = html.replace(/\{%\s*include footer\.html\s*%\}/g, footer);
+  html = html.replace(/\{%\s*include google-analytics\.html\s*%\}/g, '');
+  html = html.replace(/\{\{\s*content\s*\}\}/g, '');
+
+  // Resolve conditionals (page.title / page.description fallbacks)
+  html = html.replace(/\{%\s*if page\.title\s*%\}[\s\S]*?\{%\s*else\s*%\}([\s\S]*?)\{%\s*endif\s*%\}/g, '$1');
+  html = html.replace(/\{%\s*if page\.description[\s\S]*?\{%\s*else\s*%\}([\s\S]*?)\{%\s*endif\s*%\}/g, siteConfig.description);
+
+  // Resolve site variables
+  html = html.replace(/\{\{\s*site\.title\s*\}\}/g, siteConfig.title);
+  html = html.replace(/\{\{\s*site\.username\s*\}\}/g, siteConfig.username);
+  html = html.replace(/\{\{\s*site\.user_title\s*\}\}/g, siteConfig.user_title);
+  html = html.replace(/\{\{\s*site\.user_description\s*\}\}/g, siteConfig.user_description);
+  html = html.replace(/\{\{\s*site\.email\s*\}\}/g, siteConfig.email);
+  html = html.replace(/\{\{\s*site\.github_username\s*\}\}/g, siteConfig.github_username);
+
+  // Resolve Liquid asset path filters  e.g. {{ "/assets/css/main.css" | prepend: site.baseurl }}
+  html = html.replace(/\{\{\s*["']([^"']+)["']\s*\|\s*prepend:\s*site\.baseurl\s*\}\}/g, siteConfig.baseurl + '$1');
+  // Resolve bare string literals  e.g. {{ "assets/img/foo.png" }}
+  html = html.replace(/\{\{\s*["']([^"']+)["']\s*\}\}/g, '$1');
+
+  // Strip any remaining Liquid tags/variables
+  html = html.replace(/\{%[^%]*%\}/g, '');
+  html = html.replace(/\{\{[^}]*\}\}/g, '');
+
+  if (!fs.existsSync('_site')) fs.mkdirSync('_site');
+  fs.writeFileSync('_site/index.html', html);
+  done();
+}
+
+gulp.task('build-html', buildHtml);
+
+/*
+ * Copy compiled assets into _site so browser-sync can serve them
+ */
+gulp.task('copy-assets', function() {
+  return gulp.src('assets/**/*')
+    .pipe(gulp.dest('_site/assets/'));
 });
 
 /*
- * Rebuild Jekyll & reload browserSync
+ * Rebuild HTML & assets, then reload browserSync
  */
-gulp.task('jekyll-rebuild', gulp.series(['jekyll-build'], function (done) {
-	browserSync.reload();
-	done();
+gulp.task('jekyll-rebuild', gulp.series(['build-html', 'copy-assets'], function(done) {
+  browserSync.reload();
+  done();
 }));
 
 /*
- * Build the jekyll site and launch browser-sync
+ * Build the site and launch browser-sync
  */
-gulp.task('browser-sync', gulp.series(['jekyll-build'], function(done) {
-	browserSync({
-		server: {
-			baseDir: '_site'
-		}
-	});
-	done()
+gulp.task('browser-sync', gulp.series(['build-html', 'copy-assets'], function(done) {
+  browserSync({
+    server: {
+      baseDir: '_site'
+    }
+  });
+  done();
 }));
 
 /*
